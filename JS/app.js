258f0bf34dcb2abd5077df7b0f2e44291591e06f
Trave <trave9360@gmail.com>
2016-01-24 10:06:34 +0900

    lastest_commit

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,6 @@
-var express = require('express');
+var dt = new Date();
+	// Mon Oct 13 2014 22:56:19 GMT+0900 (대한민국 표준시)
 
-var app = express();
 
-app.use(express.static(__dirname + '/..'));
-
-app.listen(3000,function(){
-  console.log('Server on!');
-});
+  new Date(dt.getFullYear(), dt.getMonth() + 20, dt.getDate());
+  	// Mon Jun 13 2016 00:00:00 GMT+0900 (대한민국 표준시)
